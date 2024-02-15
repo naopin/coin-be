862f525714042cfa0264763dfe492e45a2b32e75
katsuragawa naoya <katsuragawanaoya@katsuragawanoMacBook-Pro.local>
2024-02-15 23:43:51 +0900

    users拡張

--- graph/model/user.go
+++ graph/model/user.go
@@ -1,9 +1,8 @@
 package model
 
 type User struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
-	// Email    string `json:"email"`
-	// Password string `json:"password"`
-	// Role     string `json:"role"`
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
